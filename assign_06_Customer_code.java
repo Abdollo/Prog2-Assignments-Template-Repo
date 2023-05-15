a0e18f4a334fd984c0c970e73dcb5d3d57bb7955
Abdallah Ahmed <111224319+Abdollo@users.noreply.github.com>
2023-05-15 22:36:03 +0300

    Add files via upload

---
+++ assign_06_Customer_code.java
@@ -0,0 +1,13 @@
+//Abdallah ahmed
+//202106164
+package banksystem;
+public class customer extends human {
+    private int SSN;
+    private String Address;
+    private BankAccount Account;
+    public customer(String name, int age, char gender, int ssn, String address) {
+        super(name, age, gender);
+        SSN = ssn;
+        Address = address;
+    }
+}
